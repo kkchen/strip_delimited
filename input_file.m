b35522a5d0098718d7c5029a365cef8b146bfdad
Kevin K. Chen <kkchen@princeton.edu>
2013-05-14 00:09:45 -0700

    Implemented the removal of C-macro-like identifier blocks in the Python script.

--- input_file.m
+++ input_file.m
@@ -11,7 +11,15 @@ end
 function x = getRandomNumber
 %GETRANDOMNUMBER Get a random number.  Source: http://xkcd.com/221/
 
+%#ifdef FOUR
 x = 4; % Chosen by fair dice roll.  Guaranteed to be random.
+%#endif
+%#ifdef FIVE
+x = 5; % Made up in my head.  No guarantees on randomness.
+%#endif % FIVE
+%#ifndef FIVE
+fprintf('The result is guaranteed to be random.\n')
+%#endif
 %#{
 
 fprintf('%i\n', x)
--- strip_delimited.py
+++ strip_delimited.py
@@ -1,47 +1,144 @@
 #!/usr/bin/env python3
 
-"""This script reads text from the standard input.  It removes all text between
-the delimiters DELIM_OPEN and DELIM_CLOSE, and prints the result to the standard
-output.  To avoid unexpected behavior, the delimiters should appear alone in
-their own lines.
+"""This script reads text from the standard input.  It removes all text between the
+delimiters DELIM_OPEN and DELIM_CLOSE (which are '%#{' and '%#}' by default),
+and prints the result to the standard output.  To avoid unexpected behavior, the
+delimiters should appear alone in their own lines.
 
-Example (default delimiters '%#{' and '%#}'):
+Note that in DELIM_OPEN and DELIM_CLOSE, regular expression special characters
+need be escaped with a backslash.  These characters are: .^$*+?\()[]|
+
+In addition, it functions as a simple C-like macro processor in the following
+way.  If the identifier IDENTIFIER is provided with the -d flag, then code in a
+
+    %#ifdef IDENTIFIER
+    ...
+    %#endif
+
+block will be preserved (except for the %# lines), and
+
+    %#ifndef IDENTIFIER
+    ...
+    %#endif
+
+blocks will be removed.  In addition, if OTHER_IDENTIFIER is not provided as an
+identifier, then
+
+    %#ifdef OTHER_IDENTIFIER
+    ...
+    %#endif
+
+blocks will be removed, and
+
+    %#ifndef OTHER_IDENTIFIER
+    ...
+    %#endif
+
+blocks will be preserved (except for the %# lines).
+
+Multiple identifiers can be provided.  These special comments should also appear
+in their own lines.
+
+Examples
+
+Remove text between the default delimiters '%#{' and '%#}':
 
     python strip_delimited.py < input_file.m > output_file.m
 
-Example (custom delimiters '%[' and '%]'):
+Custom delimiters '%[' and '%]':
 
     python strip_delimited.py -o '%\[' -c '%\]' < input_file.m > output_file.m
 
-Note that in DELIM_OPEN and DELIM_CLOSE, regular expression special characters
-need be escaped with a backslash, as in the above example.  These characters
-are: .^$*+?\()[]|
+Remove text between the default delimiters, and define the identifiers VERBOSE
+and TEST:
 
+    python strip_delimited.py -d VERBOSE TEST < input_file.m > output_file.m
+
+Kevin K. Chen
+May 14, 2013
 """
 
 import sys
 import argparse
 import re
 
-# Parse input arguments.
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    description=__doc__,
-    epilog='Kevin K. Chen\nMay 12, 2013')
+def parse():
+    """Parse input arguments.  Return the namespace of inputs."""
 
-parser.add_argument('-o', '--open',
-                    default=r'%#{',
-                    help=r"The opening delimiter (default: '%%#{').",
-                    metavar='DELIM_OPEN')
-parser.add_argument('-c', '--close',
-                    default=r'%#}',
-                    help="The closing delimiter (default: '%%#}').",
-                    metavar='DELIM_CLOSE')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=__doc__)
 
-args = parser.parse_args()
+    parser.add_argument('-o', '--open',
+                        default=r'%#{',
+                        help="""
+The opening delimiter (default: '%%#{').  Regular expression special characters
+must be escaped.""",
+                        metavar='DELIM_OPEN')
+    parser.add_argument('-c', '--close',
+                        default=r'%#}',
+                        help="""
+The closing delimiter (default: '%%#}').  Regular expression special characters
+must be escaped.""",
+                        metavar='DELIM_CLOSE')
+    parser.add_argument('-d', '--define',
+                        nargs='+',
+                        default=[],
+                        help='Identifiers to define.',
+                        metavar='IDENTIFIER')
 
-# Read in, strip, and print the text.
-text = sys.stdin.read()
-text = re.sub(r'{}.*?{}\s?'.format(args.open, args.close), r'', text,
-              flags=re.DOTALL)
-print(text, end='')
+    return parser.parse_args()
+
+
+def process(args):
+    """Read in, process, and print the text."""
+
+    text = sys.stdin.read()
+
+    # Remove text between the delimiters.
+    text = re.sub(r'{}.*?{}\s?'.format(args.open, args.close), r'', text,
+                  flags=re.S)
+
+    # Process the %#ifdef and %#ifndef blocks.
+    text = strip_blocks(text, 'ifdef')
+    text = strip_blocks(text, 'ifndef')
+
+    # All blocks to be removed have been removed.  Remove the remaining %# block
+    # delimiters.
+    text = re.sub(r'%#(if(n|)def|endif).*\n', r'', text)
+
+    print(text, end='')
+
+
+def strip_blocks(text, block):
+    """Process ifdef and ifndef blocks, and remove them accordingly.
+
+    Return the processed text.
+
+    """
+
+    assert block in ['ifdef', 'ifndef'], \
+        'block is "{}" but must be "ifdef" or "ifndef".'.format(block)
+
+    offset = 0 # text index offset as blocks get removed.
+    # Find all instances of the block.
+    matches = re.finditer(r'%#{}[ \t]+(\S*)[\w\W]*?%#endif.*\n'.format(block),
+                          text)
+    for match in matches:
+        # See if the block needs to be removed.  \1 is the identifier.
+        do_remove = match.expand(r'\1') in args.define # For %#ifndef.
+        if block == 'ifdef':
+            do_remove = not do_remove
+
+        if do_remove:
+            # The identifier wasn't provided by the user.  Remove the block.
+            text = text[:match.start()-offset] + text[match.end()-offset:]
+            # Increase the offset by the size of this block.
+            offset += match.end() - match.start()
+
+    return text
+
+
+if __name__ == '__main__':
+    args = parse()
+    process(args)
